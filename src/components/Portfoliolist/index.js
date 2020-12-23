51ecf2eb1b528be8cfa0bd515c6a5ef499eea6b6
penaone <penaone@gmail.com>
2020-12-23 09:12:54 -0700

    cleanup files

--- src/components/Portfoliolist/index.js
+++ src/components/Portfoliolist/index.js
@@ -92,7 +92,7 @@ const PortfolioList = ({ category }) => {
   const currentProjects = projects.filter((project) => project.category === category);
   
 
-    // current project
+    
   
 
   return (
@@ -112,14 +112,6 @@ const PortfolioList = ({ category }) => {
     </div>
   );
 };
-<div>
-      {/* <a className="img-thumbnail mx-1"/'><img src={MaM} alt="Munch-A-Movie link"/></a> */}
-       {/* <a className="img-thumbnail mx-1" href='https://my-haus.herokuapp.com/'><img src={MyHaus} alt="My Haus link"height="55" width="55"/></a> */}
-       {/* <a className="img-thumbnail mx-1" href="https://penaone.github.io/passwordchallenge/"><img src={Pssword} alt="Password Generator link"height="55" width="55"/></a> */}
-       {/* <a className="img-thumbnail mx-1" href='https://penaone.github.io/food-festival/'><img src={FFest} alt="Food Festival link"height="55" width="55"/></a> */}
-       {/* <a className="img-thumbnail mx-1" href='https://penaone.github.io/NewCodingquiz'><img src={NewCode} alt="Coding Quiz link"height="55" width="55"/></a> */} 
-       {/* <a className="img-thumbnail mx-1" href='https://penaone.github.io/run-buddy'><img src={RunBdy} alt="Coding Quiz link"height="55" width="55"/></a> */}
 
-    </div>
 
 export default PortfolioList;
